ee7a70607881eb7f5778b9781d5bc535a39c1e08
dariagazizova <dgazizova@mun.ca>
2025-03-26 01:17:11 -0230

    Add CNN for mnist dataset

--- FF_number_vision/CNN_numer_vision_pytorch.py
+++ FF_number_vision/CNN_numer_vision_pytorch.py
@@ -14,18 +14,39 @@ class LossType(enum.Enum):
     MSE = "MSE"
 
 
-class FeedForwardNet(nn.Module):
+class ConvolutionaldNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.output = nn.Sequential(
-            nn.Linear(784, 30),
-            nn.Sigmoid(),
+
+        self.conv_layers = nn.Sequential(
+            # (28, 28), 1 -> 16 filters -> 28 + 2*p - (k-1) / s
+            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            # 28 / s = 14 -> (14, 14)
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # (14, 14), 16 -> 32 filters
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            # (14, 14) -> (7, 7)
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc_layers = nn.Sequential(
+            # flatten before, get 32*7*7
+            nn.Linear(32*7*7, 30),
+            nn.ReLU(),
             nn.Linear(30, 10),
             nn.Sigmoid()
         )
 
-    def forward(self, x):
-        return self.output(x)
+    def forward(self, X):
+        # add channel so shape becomes (batch_size, 1, 28, 28), n_channels = 1
+        X = X.unsqueeze(1)
+        X = self.conv_layers(X)
+
+        # flattens image into a vector before going into fully connected layers
+        X = X.reshape(X.size(0), -1)
+        return self.fc_layers(X)
 
 
 def train(model, loss_type: LossType, learning_rate, epochs, train_dataset, test_dataset):
@@ -42,21 +63,21 @@ def train(model, loss_type: LossType, learning_rate, epochs, train_dataset, test
     for epoch in range(epochs):
         model.train()
         total_loss = 0
-        for X, Y in train_dataset:
+        for X, Y in train_dataset:  # for batches
             y_target = torch.nn.functional.one_hot(Y, num_classes=10).float()
 
             output = model(X)
             loss = loss_fn(output, y_target)
-            optimizer.zero_grad()  # to clean gradient values
+            # to clean gradient values
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
             total_loss += loss.item()
 
         loss_epochs.append(total_loss / len(train_dataset))
 
-        if epoch % 10 == 0:
-            print(f"Epoch {epoch}: Loss = {loss_epochs[-1]:.4f}")
-            evaluate(model, test_dataset)
+        print(f"Epoch {epoch}: Loss = {loss_epochs[-1]:.4f}")
+        evaluate(model, test_dataset)
 
     return loss_epochs
 
@@ -65,7 +86,7 @@ def evaluate(model, test_dataset):
     model.eval()
     correct, total = 0, 0
     with torch.no_grad():
-        for X, Y in test_dataset:
+        for X, Y in test_dataset:  # for batches
             output = model(X)
             preds = output.argmax(dim=1)
             correct += (preds == Y).sum().item()
@@ -75,8 +96,9 @@ def evaluate(model, test_dataset):
 
 # get train and test data
 train_images, train_labels, test_images, test_labels = get_images()
-train_images = train_images.reshape(train_images.shape[0], -1) / 255  # scaling from [0, 255], to [0, 1]
-test_images = test_images.reshape(test_images.shape[0], -1) / 255
+# scaling from [0, 255], to [0, 1]
+train_images = train_images / 255
+test_images = test_images / 255
 
 X_tensor = torch.tensor(train_images, dtype=torch.float32)
 Y_tensor = torch.tensor(train_labels.T, dtype=torch.long)
@@ -84,14 +106,14 @@ Y_tensor = torch.tensor(train_labels.T, dtype=torch.long)
 X_test_tensor = torch.tensor(test_images, dtype=torch.float32)
 Y_test_tensor = torch.tensor(test_labels.T, dtype=torch.long)
 
-train_dataset = DataLoader(TensorDataset(X_tensor, Y_tensor), batch_size=10, shuffle=True)
+train_dataset = DataLoader(TensorDataset(X_tensor, Y_tensor), batch_size=5, shuffle=True)
 test_dataset = DataLoader(TensorDataset(X_test_tensor, Y_test_tensor), batch_size=1000, shuffle=False)
 
 
 loss_type = LossType.MSE
-model = FeedForwardNet()
-learning_rate = 2.0 if loss_type == LossType.MSE else 0.1
-epochs = 201
+model = ConvolutionaldNet()
+learning_rate = 0.1 if loss_type == LossType.MSE else 0.01
+epochs = 20
 loss = train(
     model, loss_type=loss_type, learning_rate=learning_rate, epochs=epochs,
     train_dataset=train_dataset, test_dataset=test_dataset
@@ -102,14 +124,14 @@ plt.plot(loss)
 plt.title(f"Loss using {loss_type.value}")
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
-plt.savefig(f"Loss_using_{loss_type.value}_torch.png")
+plt.savefig(f"Loss_using_{loss_type.value}_torch_CNN.png")
 plt.show()
 
 
 loss_type = LossType.NLL
-model = FeedForwardNet()
-learning_rate = 2.0 if loss_type == LossType.MSE else 0.1
-epochs = 201
+model = ConvolutionaldNet()
+learning_rate = 0.1 if loss_type == LossType.MSE else 0.1
+epochs = 20
 loss = train(
     model, loss_type=loss_type, learning_rate=learning_rate, epochs=epochs,
     train_dataset=train_dataset, test_dataset=test_dataset
@@ -120,7 +142,7 @@ plt.plot(loss)
 plt.title(f"Loss using {loss_type.value}")
 plt.xlabel("Epoch")
 plt.ylabel("Loss")
-plt.savefig(f"Loss_using_{loss_type.value}_torch.png")
+plt.savefig(f"Loss_using_{loss_type.value}_torch_CNN.png")
 plt.show()
 
 
